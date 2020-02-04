34a585ee18d6f779833f69a8033fe775e15aa161
Marco Apraez <maz_2002@email.com>
2020-02-04 08:35:56 -0800

    added page title variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
 
+  before_action :set_title
+  
+  def set_title
+    @page_title = "Devcamp Portfolio | My Portfolio Website"
+
+  end
+
+
 end
